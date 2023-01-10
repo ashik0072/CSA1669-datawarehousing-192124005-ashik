925086d04a87e7780d3fde968c18b6f30dbc0049
ashik0072 <114140750+ashik0072@users.noreply.github.com>
2023-01-10 14:49:35 +0530

    Add files via upload

---
+++ DAY2 [10-1-2023]/decimalscaling.r
@@ -0,0 +1,4 @@
+diabetes=read.csv("diabetes.csv")
+A=c(diabetes$Age)
+Decimalscaling =(A/100)
+View(Decimalscaling)
---
+++ DAY2 [10-1-2023]/linear regression.r
@@ -0,0 +1,7 @@
+data1=read.csv("diabetes.csv")
+relation=lm(data1$BloodPressure~data1$Age)
+Png=(file="linear regression.png")
+plot(data1$Age,data1$BloodPressure,col="green", main="Linear Regression Analysis",abline= (lm(data1$BloodPressure~ data1$Age)), xlab="BloodPressure",ylanb= "Age")
+A=data.frame(data1$Age)
+Result=predict(relation,A)
+Print(Result)
